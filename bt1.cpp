4e37064e8cf514b8fcf2470d620ec2d037d5ab69
NgoNhatWin <117475856+ngonhatwin@users.noreply.github.com>
2022-11-21 07:39:50 +0700

    Add files via upload

---
+++ bt1.cpp
@@ -0,0 +1,13 @@
+#include "stdio.h"
+int main () {
+
+int a ;
+printf ("\n hay nhap so nguyen bat ki ");
+scanf ("%d", &a);
+if (a<0) {
+	printf ("\n a la so am", a);
+}else{
+	printf ("\n a la so duong");
+}
+
+}
